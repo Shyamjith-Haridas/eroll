a1540f3b38b864eda53fa60ee9c846950c567516
Shyamjith Haridas <sreeshyam2@gmail.com>
2025-07-28 10:37:29 +0530

    ERL - 05 | Firebase Integration and UI almost completed.

--- lib/main.dart
+++ lib/main.dart
@@ -1,9 +1,16 @@
 import 'package:eroll/core/routes/app_route_config.dart';
 import 'package:eroll/core/routes/app_route_names.dart';
 import 'package:eroll/core/themes/app_themes.dart';
+import 'package:eroll/firebase_options.dart';
+import 'package:firebase_core/firebase_core.dart';
 import 'package:flutter/material.dart';
 
-void main() {
+void main() async{
+  WidgetsFlutterBinding.ensureInitialized();
+  await Firebase.initializeApp(
+    options: DefaultFirebaseOptions.currentPlatform,
+  );
+
   runApp(const MyApp());
 }
 
